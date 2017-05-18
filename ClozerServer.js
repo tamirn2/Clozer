33a3226d93c40cf7575e8f1fbb33ed8efc1d54b4
user <tamirn2@gmail.com>
2017-05-19 00:02:33 +0300

    you need this

--- ClozerServer.js
+++ ClozerServer.js
@@ -23,7 +23,7 @@ io.on('connection', function (socket) {
     var room = socket.handshake['query']['room_var'];
     console.log("rooms we defined",rooms[room]);
 
-    console.log("room",room);
+    console.log("room debug:",room);
      socket.join(room);
      console.log('user joined room #'+room);
 
